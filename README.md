6ecb8f809a57d276b535babe4c9e9c8be7e557e8
Arkadiusz Nowakowski <nowakowski.arkadiusz@gmail.com>
2020-06-19 12:57:44 +0200

    feat: finishing setup

---
+++ README.md
@@ -0,0 +1,6 @@
+# Coding Dojo Silesia Kata Installer
+
+Run the following code:
+```
+npm init @coding-dojo-silesia/gameoflife-kata [{folderNameForKata}]
+```
--- bin/setup.js
+++ bin/setup.js
@@ -30,23 +30,41 @@ async function setup()
 {
     try {
 
-        console.log(`Cloning Kata files from the repository ${repositoryUrl}`);
+        console.log();
+        console.log('.-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-.');
+        console.log(`| Coding Dojo Silesia Kata Installer                |`);
+        console.log('`-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-`');
+
+        console.log();
+        console.log('.-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-.');
+        console.log(`| Cloning Kata files from the repository...         |`);
         await runShellCmd(`git clone --depth 1 --no-checkout ${repositoryUrl} ${kataPath}`);
         process.chdir(kataPath);
         await runShellCmd(`git checkout master -- kata-boilerplate/`);
         rimraf.sync(path.join(kataPath, ".git"));
         copydir.sync(path.join(kataPath, "kata-boilerplate"), kataPath);
         rimraf.sync(path.join(kataPath, "kata-boilerplate"));
+        console.log(`| ...the kata is cloned!                            |`);
+        console.log('`-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-`');
   
-        console.log(`Installing dependencies, please wait...`);
-        await runShellCmd(`npm i`);
-        console.log(`All dependencies are installed successfully!`);
         console.log();
-        console.log(`First of all, go to the kata folder:`);
+        console.log('.-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-.');
+        console.log(`| Installing dependencies, please wait...           |`);
+        await runShellCmd(`npm i`);
+        console.log(`| ...all dependencies are installed successfully!   | `);
+        console.log('`-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-`');
+
+        console.log();
+        console.log('.-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-.');
+        console.log(`| Read the introduction below:                      |`);
+        console.log('`-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-`');
+
+        console.log();
+        console.log(`(1) First of all, go to the kata folder:`);
         console.log();
         console.log(`cd ${kataDirName}`);
         console.log();
-        console.log(`Secondly, there are several commands you can use:`);
+        console.log(`(2) Secondly, there are several commands you can use:`);
         console.log();
         console.log(`npm run kata`);
         console.log(`// ^-- show the kata description`);
@@ -56,7 +74,11 @@ async function setup()
         console.log();
         console.log(`npm test`);
         console.log(`// ^-- the MOST important command, you should use it very often`);
+
         console.log();
+        console.log('.-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-.');
+        console.log(`| You ready to go! Good luck!                       |`);
+        console.log('`-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-`');
   
     } catch (error) {
         console.log(error);
