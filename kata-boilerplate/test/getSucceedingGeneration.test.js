ff06590fc7b26dbb33775bd7cc74cec238010e2a
Arkadiusz Nowakowski <nowakowski.arkadiusz@gmail.com>
2020-06-19 13:27:52 +0200

    feat: add example test case

---
+++ kata-boilerplate/test/getSucceedingGeneration.test.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const getSucceedingGeneration = require('../src/getSucceedingGeneration.js');
+
+test.each([
+    [fs.readFileSync(__dirname + '/examples/example01-input.txt', 'utf8'), fs.readFileSync(__dirname + '/examples/example01-output.txt', 'utf8')],
+])('if the evolved second generation is valid', (initialGenerationConfig, expectedOutput) => {
+    expect(getSucceedingGeneration(initialGenerationConfig, 1)).toBe(expectedOutput);
+});
