178e469b77d1b832d9824998cd72f11f64e804d3
Arkadiusz Nowakowski <nowakowski.arkadiusz@gmail.com>
2020-07-09 21:20:34 +0200

    My kata code

--- kata-boilerplate/src/getSucceedingGeneration.js
+++ kata-boilerplate/src/getSucceedingGeneration.js
@@ -1,6 +1,44 @@
-function getSucceedingGeneration(initialGenerationConfig, evolveByNGenerations = 1)
+function exist(generation, x, y)
 {
-    return initialGenerationConfig;
+    return typeof generation[y] !== 'undefined' && typeof generation[y][x] !== 'undefined';
 }
 
-module.exports = getSucceedingGeneration;
+function isAlive(generation, x, y)
+{
+    return generation[y][x] === '*';
+}
+
+function getNofAliveNeighbours(generation, x, y)
+{
+    return [[-1,-1],[0,-1],[1,-1],[-1,0],[1,0],[-1,1],[0,1],[1,1]].reduce((counter, offset) => {
+        return counter + (exist(generation, x + offset[0], y + offset[1]) && isAlive(generation, x + offset[0], y + offset[1]) ? 1 : 0)
+    }, 0);
+}
+
+function getSucceedingGeneration(initialGenerationConfig, evolveByNGenerations = 1)
+{
+    return [...Array(evolveByNGenerations)]
+        .reduce(
+            generation => { // evolving
+                return generation.map((row,y) => row.map((cell,x) => {
+                    let nofAliveNeighbours = getNofAliveNeighbours(generation, x, y);
+                    return cell === '.'
+                        ? (nofAliveNeighbours === 3 ? '*' : '.')
+                        : (nofAliveNeighbours === 2 || nofAliveNeighbours === 3 ? '*' : '.');
+                }));
+            },
+            initialGenerationConfig.split(/\r?\n/)
+                .map(line => line.trim()) // remove irrelevant characters
+                .slice(1) // f*** the grid size
+                .map(line => line.split(''))
+        )
+        .map(line => line.join(''))
+        .join('\r\n');
+}
+
+module.exports = {
+    exist,
+    isAlive,
+    getNofAliveNeighbours,
+    getSucceedingGeneration
+};
--- kata-boilerplate/test/getSucceedingGeneration.test.js
+++ kata-boilerplate/test/getSucceedingGeneration.test.js
@@ -1,8 +1,27 @@
 const fs = require('fs');
-const getSucceedingGeneration = require('../src/getSucceedingGeneration.js');
+const { exist, isAlive, getNofAliveNeighbours, getSucceedingGeneration } = require('../src/getSucceedingGeneration.js');
+
+test('if exist function check the cell existence', () => {
+    expect(exist([['.','.'],['.','*']], 1, 1)).toBeTruthy();
+    expect(exist([['.','.'],['.','*']], 2, 1)).toBeFalsy();
+});
+
+test('if isAlive function check the cell pzazz', () => {
+    expect(isAlive([['.','.'],['.','*']], 1, 1)).toBeTruthy();
+    expect(isAlive([['.','.'],['.','*']], 0, 1)).toBeFalsy();
+});
+
+test('if getNofAliveNeighbours function returns the correct number of alive neighbours', () => {
+    expect(getNofAliveNeighbours([['.','.'],['.','*']], 1, 1)).toBe(0);
+    expect(getNofAliveNeighbours([['.','.'],['.','*']], 0, 1)).toBe(1);
+});
 
 test.each([
-    [fs.readFileSync(__dirname + '/examples/example01-input.txt', 'utf8'), fs.readFileSync(__dirname + '/examples/example01-output.txt', 'utf8')],
-])('if the second generation is valid', (initialGenerationConfig, expectedOutput) => {
-    expect(getSucceedingGeneration(initialGenerationConfig, 1)).toBe(expectedOutput);
+    [fs.readFileSync(__dirname + '/examples/example01-input.txt', 'utf8'), fs.readFileSync(__dirname + '/examples/example01-output.txt', 'utf8'), 1],
+    [fs.readFileSync(__dirname + '/examples/example02-input.txt', 'utf8'), fs.readFileSync(__dirname + '/examples/example02-output.txt', 'utf8'), 2],
+    [fs.readFileSync(__dirname + '/examples/example03-input.txt', 'utf8'), fs.readFileSync(__dirname + '/examples/example03-output.txt', 'utf8'), 11],
+])('if the second generation is valid', (initialGenerationConfig, expectedOutput, evolveByNGenerations) => {
+    let output = getSucceedingGeneration(initialGenerationConfig, evolveByNGenerations);
+    console.log(output);
+    expect(output).toBe(expectedOutput);
 });
